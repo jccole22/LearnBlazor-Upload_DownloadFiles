5b3e3afb6b375e31348f8d21c31baa502398929c
Jonah <jonahcole@yahoo.com>
2022-11-17 17:37:28 -0800

    Add functionality to preview images when uploading

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using LearnBlazorFileUpload.Data;
 using LearnBlazorFileUpload.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<IFileUpload, FileUpload>();
 
 var app = builder.Build();
